6870556fc1d6012b696d91f34bd1889ba64f95d9
Rosalyn <amondiajigoh@gmail.com>
2024-10-24 19:53:52 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,74 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Todo List</h1>
+    <input type="text" v-model="newTodo" @keyup.enter="addTodo" placeholder="Add a new task" />
+    <button @click="addTodo">Add</button>
+
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        <input type="checkbox" v-model="todo.completed" />
+        <span :class="{ completed: todo.completed }">{{ todo.text }}</span>
+        <button @click="removeTodo(index)">Remove</button>
+      </li>
+    </ul>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      newTodo: "", // Stores the new task input
+      todos: []    // Stores the list of tasks
+    };
+  },
+  methods: {
+    addTodo() {
+      // Adds a new todo to the list
+      if (this.newTodo.trim() !== "") {
+        this.todos.push({ text: this.newTodo.trim(), completed: false });
+        this.newTodo = ""; // Clear the input field
+      }
+    },
+    removeTodo(index) {
+      // Removes a todo from the list
+      this.todos.splice(index, 1);
+    }
   }
-}
+};
 </script>
-
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Arial, sans-serif;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+input[type="text"] {
+  margin-bottom: 10px;
+  padding: 5px;
+  width: 200px;
+}
+
+button {
+  margin-left: 5px;
+  padding: 5px 10px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 10px 0;
+}
+
+.completed {
+  text-decoration: line-through;
+  color: gray;
 }
 </style>
